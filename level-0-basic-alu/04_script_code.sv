c90e0b90e5b066c49e64dc4dc1407372ac53f4a9
G R Gunashree <162901136+gunashreeramesh@users.noreply.github.com>
2026-08-28 21:55:41 +0530

    Add synthesis script for parameterized ALU

--- level-0-basic-alu/04_script_code.sv
+++ level-0-basic-alu/04_script_code.sv
@@ -1,1 +1,27 @@
+# Set library
+set_db library /path/to/your/standard_cell_library.lib
 
+# Read RTL
+read_hdl parameterized_alu.v
+
+# Elaborate design
+elaborate parameterized_alu
+
+# Read constraints
+read_sdc input_constraints.sdc
+
+# Synthesis
+syn_generic
+syn_map
+syn_opt
+
+# Reports
+report_timing > timing_report.txt
+report_area > area_report.txt
+report_power > power_report.txt
+
+# Write synthesized netlist
+write_hdl > synthesized_alu.v
+
+# Write constraints
+write_sdc > output_constraints.sdc
